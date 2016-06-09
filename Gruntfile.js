c4442e1c636d1946739ba69714bc72c1f2385b3d
Jenn Schiffer <jenn@dotbiz.info>
2016-06-09 11:43:08 -0400

    add eslint, grunt tooling

---
+++ Gruntfile.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var spawn = require('child_process').spawn;
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    eslint: {
+      src: {
+        options: {
+          configFile: '.eslintrc-es2015.yaml',
+        },
+        src: 'src/**/*.js',
+      },
+      root: {
+        options: {
+          configFile: '.eslintrc-node.yaml',
+        },
+        src: '*.js',
+      },
+      test: {
+        options: {
+          configFile: '.eslintrc-mocha.yaml',
+        },
+        src: 'test/**/*.js',
+      },
+    },
+    watch: {
+      options: {
+        spawn: false,
+      },
+      config: {
+        files: ['.env', 'config.js'],
+        tasks: ['kill', 'start'],
+      },
+      src: {
+        files: ['<%= eslint.src.src %>'],
+        tasks: ['eslint:src', 'mochaTest', 'kill', 'start'],
+      },
+      root: {
+        files: ['<%= eslint.root.src %>'],
+        tasks: ['eslint:root'],
+      },
+      test: {
+        files: ['<%= eslint.test.src %>'],
+        tasks: ['eslint:test', 'mochaTest'],
+      },
+      lint: {
+        options: {
+          reload: true,
+        },
+        files: ['.eslintrc*', 'eslint/*'],
+        tasks: ['eslint'],
+      },
+      // Reload the bot if chatter files change. This makes dev MUCH easier!
+      chatter: {
+        files: ['node_modules/chatter/dist/**/*'],
+        tasks: ['kill', 'start'],
+      },
+    },
+  });
+
+  grunt.registerTask('start', function() {
+    global._BOT = spawn('node', ['--require', 'babel-register', './app'], {stdio: 'inherit'});
+  });
+
+  grunt.registerTask('kill', function() {
+    global._BOT.kill('SIGKILL');
+  });
+
+  grunt.registerTask('test', ['eslint', 'mochaTest']);
+  grunt.registerTask('default', ['start', 'watch']);
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-eslint');
+};
--- config.js
+++ config.js
@@ -12,6 +12,6 @@ module.exports = {
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
   },
 };
