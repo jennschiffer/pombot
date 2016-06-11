005774d66e6095c9d6c97497929d25e63be61369
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2016-06-11 12:57:34 +0100

    Update index.js

--- src/sql/index.js
+++ src/sql/index.js
@@ -1,13 +1,16 @@
 import {QueryFile, utils} from 'pg-promise';
 
-// build an object with keys for each query.
-// names with _ get camel cased (e.g. find_by_id becomes findById)
+// Here we build a camelized tree with file paths for values, using camelizeVar,
+// and then replace each path with a QueryFile object.
+// See the API:
+// http://vitaly-t.github.io/pg-promise/utils.html#.enumSql
+// http://vitaly-t.github.io/pg-promise/utils.html#.camelizeVar
 const queries = utils.enumSql(__dirname, {}, file => {
   return QueryFile(file, { // eslint-disable-line new-cap
     minify: true,
     // will automatically update if file time changes without
     // having to restart the process
-    debug: process.env.DEBUG,
+    debug: process.env.DEBUG
   });
 });
 
