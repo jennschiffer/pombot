770e92a747825738f88ffed90f99fc564bc9dbf0
Jenn Schiffer <jenn@dotbiz.info>
2016-06-15 17:49:34 -0400

    show time left in status
    stub out iwill command

--- src/pombot/commands/iwill.js
+++ src/pombot/commands/iwill.js
@@ -4,57 +4,55 @@
 import {createCommand} from 'chatter';
 import lookupPom from '../lib/lookup-pom';
 import {one} from '../../services/db';
+import getTimeString from '../lib/get-time-string';
+
+// helper to create task
+const addTaskToPom = function(pomId, userId) {
+
+  return userId;
+  // if user exists, see if they have a task
+    // if they have a task, update with message
+    // else add task
+
+  // else add user
+    // add task
+
+  /* return query.createTask({
+      pom_id: pomId,
+      slack_user_id: userId,
+      description: message
+    }).then(newTaskRes => {
+      return `${user.name}'s task for the next pom is "${message}". You can start this pom with the command \`${getCommand('start')}\``;
+    });*/
+};
 
 export default createCommand({
   name: 'i will',
   description: 'Records a users task for the next pom.',
 }, (message, {user, channel, token, getCommand}) => {
 
-  // look up pom
-  return lookupPom(token, channel.id).then(res => {
+// look up pom
+  return lookupPom(token, channel.id).then(pomId => {
 
-    // if pom exists, TODO check if running and if not add the task
-    if (res) {
-      return `pom already exists with id ${res}`;
-      // return `:tomato: the pom has been stopped with *Z* remaining.`;
+    // if pom exists check if it is already running or update with start time
+    if (pomId) {
+      return one.getPomById({pomId}).then(pomRes => {
+
+        // if pom exists and it's running, let user know they can't update tasks
+        if (pomRes.started_at && !pomRes.is_completed) {
+          const timeLeft = getTimeString(pomRes.date_part);
+          return `it's too late to declare a task, a pom is already running with *${timeLeft}* left.`;
+        }
+
+        // add task to this unstarted pom ID
+        return addTaskToPom(pomId, user.id);
+      });
     }
 
-    // if pom doesn't exist, TODO create and then add task
-    return one.createPom({slack_channel_id: channel.id}).then(newPom => {
-      return `pom just created with id ${newPom.id}`;
-      // return `:tomato: pom started – you have *Z* left!`;
+    // if pom doesn't exist, create pom (but don't start it) and then add task to it
+    return one.createPom({slack_channel_id: channel.id}).then(newPomId => {
+      return addTaskToPom(newPomId, user.id);
     });
   });
-
-/*  db.query('SELECT * FROM pom WHERE id=$1', pomId, 1).then(result => {
-
-    if (result.startedAt && !result.isComplete) {
-      // TODO get the time left Z in current pom
-      return `it's too late to declare a task, a pom is running with *Z* left.`;
-    }
-
-    // if pom is not running or is on break, let user record task
-
-
-      4. user.name enters a pom_task with the description message
-
-      check if record for user's slack_user id exists
-        if so, get slack_user_id
-
-        if not, enter it
-        INSERT INTO slack_user (slack_user_id, slack_team_id) VALUES ([slack_user_id], [slack_team_id]);
-
-      check if pom_task with this slack_user_id and pom_id exist
-        if so, update with new description value
-
-        if not, enter it
-        INSERT INTO pom_task (pom_id, slack_user_id, description) VALUES ([pom_id], [slack_user_id], message);
-
-    return heredoc.oneline.trim`
-      ${user.name}'s task for the next pom is "${message}".
-      You can start this pom with the command \`${getCommand('start')}\`
-    `;
-  });
-  */
 
 });
--- src/pombot/commands/status.js
+++ src/pombot/commands/status.js
@@ -2,7 +2,9 @@
  * The `status` command, which describes the status of a current Pom.
  */
 import {createCommand} from 'chatter';
+import {one, query} from '../../services/db';
 import lookupPom from '../lib/lookup-pom';
+import getTimeString from '../lib/get-time-string';
 
 export default createCommand({
   name: 'status',
@@ -10,26 +12,31 @@ export default createCommand({
 }, (message, {channel, token, getCommand}) => {
 
   // look up pom
-  return lookupPom(token, channel.id).then(res => {
+  return lookupPom(token, channel.id).then(pomId => {
 
-    // if pom exists, TODO get status
-    if (res) {
-      return `status of pom: id is ${res}`;
+    // if pom exists, show time left and any tasks for it
+    if (pomId) {
 
-      /*
-        // retrieve any tasks in this pom.
-        const taskList = [];
-        // for (const user in pom.taskCollection) {
-        //  taskList.push(`> ${user} will ${pom.taskCollection[user]}`);
-        // }
-        const taskHeader = (taskList.length > 0) ? 'here is the task list:' : 'there are no tasks declared.';
+      // TODO retrieve any tasks that exist for this pomId
+      const taskList = [];
+      // for (const user in pom.taskCollection) {
+      //  taskList.push(`> ${user} will ${pom.taskCollection[user]}`);
+      // }
+      const taskHeader = (taskList.length > 0) ? 'here is the task list:' : 'there are no tasks declared.';
 
-        if (result.startedAt && !result.isComplete) {
-          // get the time left Z in current pom
-          // get the list of tasks in the current pom
-          return `a pom is currently running with *Z* left and ${taskHeader}`;
+
+      return one.getPomById({pomId}).then(pomRes => {
+
+        if (pomRes.started_at && !pomRes.is_completed) {
+          const timeLeft = getTimeString(pomRes.date_part);
+          return `a pom is currently running with *${timeLeft}* left and ${taskHeader}`;
         }
-      */
+
+        return query.startPom({slack_channel_id: channel.id}).then(startRes => {
+          const timeLeft = getTimeString(startRes.date_part);
+          return `:tomato: pom started – you have *${timeLeft}* left!`;
+        });
+      });
     }
 
     // there's no existing pom to check the status of
